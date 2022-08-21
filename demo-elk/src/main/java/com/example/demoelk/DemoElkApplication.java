f7f18999fd129e1cc69b9ec7f137903e5a88146e
huytv <huy8895@gmail.com>
2022-08-21 14:51:52 +0700

    [done] create image from spring boot. write log in server. read log by logstash and view by kibana

--- demo-elk/src/main/java/com/example/demoelk/DemoElkApplication.java
+++ demo-elk/src/main/java/com/example/demoelk/DemoElkApplication.java
@@ -1,19 +1,29 @@
 package com.example.demoelk;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @Slf4j
 @RestController
 public class DemoElkApplication {
+	@Value("${logging.file.name}")
+	private String LOGGING_FILE;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoElkApplication.class, args);
+	}
+
+	@PostConstruct
+	void logConfig(){
+		log.info("LOGGING_FILE: {}", LOGGING_FILE);
 	}
 
 	@GetMapping("ping/{name}")
